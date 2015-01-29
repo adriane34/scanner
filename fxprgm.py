03ea9f6d96e24731e400c93ae0077ed9a1cdab68
adriane34 <adriane.aytona@yahoo.com>
2015-01-29 14:50:39 -0600

    Modify fxprgm.py

--- fxprgm.py
+++ fxprgm.py
@@ -10,6 +10,8 @@ get_token_url = "http://pi.shahrdar.com/api/APIAuthentication/Get"
 query_currency_url = "http://pi.shahrdar.com/api/MobileService/Query"
 scan_currency_url = "http://pi.shahrdar.com/api/MobileService/Seize"
 rem_currency_url = "http://pi.shahrdar.com/api/MobileService/Delete"
+bdg_number_url = "http://pi.shahrdar.com/api/APIAuthentication/BadgeCheck"
+cs_number_url = "http://pi.shahrdar.com/api/APIAuthentication/CaseCheck"
 
 token_payload = {"API":"ced040d6-dfbe-4b72-9ba7-a918fc6a5632","ORI":ORI}
 
@@ -37,7 +39,19 @@ def rem_currency( rem_currency_payload):
         rem_currency_results = requests.post(url=rem_currency_url,data=json.dumps(rem_currency_payload), headers=headers)
         _rem_currency_result = json.loads(rem_currency_results.text)
         return _rem_currency_result;
-        
+
+# Checks Badge Number
+def bdg_number(badgeNumber):
+        bdg_number_results = requests.get(url=bdg_number_url,data=json.dumps(badgeNumber), headers=headers)
+        _bdg_number_result = json.loads(bdg_number_results.text)
+        return _bdg_number_result;
+
+# Checks Case Number
+def cs_number(bdg_number_payload):
+        bdg_number_results = requests.get(url=cs_number_url,data=json.dumps(_case), headers=headers)
+        _bdg_number_result = json.loads(bdg_number_results.text)
+        return _bdg_number_result;
+
 _token = get_Token(token_payload)
 
 with open(r"C:\Users\kennethaytona\Desktop\SS\rqconf.png", "rb") as imageFile:
@@ -53,12 +67,16 @@ while True:
 
         if selection == 2:
                 _case = input("Enter Case Number: ")
+                _case_number = {"Case":_case,"token":_token}
+                print (cs_number(_case_number))
                 badgeNumber = input("Enter Badge Number: ")
+                _badge_number = {"Badge Number":badgeNumber,"token":_token}
+                print (bdg_number(_badge_number))
                 _scan_currency_payload = { "ORI":ORI, "Token":_token, "Case":_case,"BadgeNumber":badgeNumber, "currencies":[{"SerialNumber":"LH023456765F","Denomination":"100","Date":st,"Photo":"strg"}]}
                 print (scan_currency( _scan_currency_payload ))
 
         if selection == 3:
                 SerialNumber=input("Please enter the serial number: ")
                 Denomination=input("Please enter the denomination: ")
-                _remove_currency_payload = { "currencies":[{"SerialNumber":SerialNumber,"Denomination":Denomination}],"token":_token}
+                _remove_currency_payload = { "currencies":[{"SerialNumber":SerialNumber,"Denomination":Denomination}],"token":_token}
                 print (rem_currency(_remove_currency_payload))
